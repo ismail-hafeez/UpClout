8c7906ce08711a3d7e8deec2a3b33f08cbc85ec1
Ismail Hafeez <ismailhafeez13@gmail.com>
2025-10-18 11:32:01 +0500

    separate log class to log

--- src/log.py
+++ src/log.py
@@ -1,8 +1,59 @@
 from datetime import datetime
 
+# -- create_database.py -- #
 def log_db_donfig(message: str) -> None:
-    PATH="../logs"
+    PATH = "../../logs/db_config"
     # Writing to log file
-    with open(f"{PATH}/db_config.log", "a", encoding="utf-8") as file:
+    with open(f"{PATH}/create_database.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+# -- load.py -- #
+def log_influencer_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/influencer.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+def log_brand_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/brand.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+def log_hashtags_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/hashtags.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+def log_posts_hashtags_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/posts_hashtags.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+def log_posts_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/posts.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+def log_taggedUsers_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/taggedUsers.log", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] - {message}\n")
+
+def log_posts_taggedUsers_table(message: str) -> None:
+    PATH = "../logs/load"
+    # Writing to log file
+    with open(f"{PATH}/posts_taggedUsers.log", "a", encoding="utf-8") as file:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"[{timestamp}] - {message}\n")
