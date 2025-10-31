070927ec6aaefa4a7d4043243381f21bab3c1c84
Ismail Hafeez <ismailhafeez13@gmail.com>
2025-10-31 16:57:07 +0500

    before mid eval

--- src/sql_temp.sql
+++ src/sql_temp.sql
@@ -6,10 +6,10 @@ JOIN posts p
     ON p.postid = ph.post_id
 JOIN influencers i
     ON i.influencerid = p.ownerid
-WHERE i.username = 'ayna.sulaiman'
+WHERE i.username = 'inaseemshah'
 GROUP BY h.tag_name
 ORDER BY use_count DESC
 LIMIT 5;
 
 SELECT *
-FROM influencers
+FROM influencers;
